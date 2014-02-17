410847d255b5d2a3045995ccca4c39fdc841ea39
Josh Kennedy <kennedy.josh@gmail.com>
2014-02-16 21:43:09 -0700

    adding some ubuntu bootstrap

---
+++ bootstrap/os/linux.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# do OS detection (ubuntu, redhat, etc)
+if [ -e "$(which lsb_release)" ]
+then
+  osname=$(lsb_release -i | cut -d ":" -f 2 | awk '{print $1}')
+
+  if [ "$osname" == "ubuntu" ]
+  then
+  fi
+fi
+
+bash $SYMPHONY/terminal/configure.sh
